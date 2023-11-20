e5db2aeb9b250b328398c20d3348995db6dffffa
n02b3rt <theonionman627@gmail.com>
2023-11-20 21:38:14 +0100

    A few changes and the addition of a form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,29 @@
+import React, {useState} from 'react';
+// useState - react Hook
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.expenses.title);
+  // przyjmuje zmienną, a druga wartość jest to funkcja wykonująca
+
+  // let title = props.expenses.title;
+  // Dobrą praktyką jest to że piszemy slowo Handler - jeśli funkcja jest wywoływana przez jakiś event aby łatwiej bylo czytac kod
+  const clickHandler = () =>{
+    setTitle('Updated!');
+    console.log(title);
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.expenses.date} />
       <div className="expense-item__description">
-        <h2>{props.expenses.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.expenses.amount}</div>
       </div>
+      {/* nie wolno wpisywać fukcji tak: funkcja() ponieważ te nawiasy sprawiają że fukncja zostaje wywołana jeśli nie damy tych nawiasów to tworzymy wskaźnik na fukcję która zadziała jak klikniemy na przycisk */}
+      <button onClick={clickHandler}> Change Title</button>
     </Card>
   );
 }
